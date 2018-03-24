de2fe8f43832849ab7c77c7a9e28322e7dccf95a
Ali Almohaya <alialmohaya@gmail.com>
2018-03-24 13:03:39 +0300

    fix

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000;
 
 const setBeatAPIUrl = 'https://api-beta.setbeat.com';
 
+app.use(cros());
+
 app.get('/', (req, res) => {
     res.end('working');
 });
@@ -14,7 +16,5 @@ app.use('/v1', proxy({
     target: setBeatAPIUrl,
     changeOrigin: true
 }));
-
-app.use(cros());
 
 app.listen(port, () => console.log('the server is running at port =>' + port));
