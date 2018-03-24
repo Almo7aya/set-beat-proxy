3c36b1124e5f8881386878974787c42f42d65b85
Ali Almohaya <alialmohaya@gmail.com>
2018-03-24 12:50:21 +0300

    add the proxy

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const app = require('express')();
 const proxy = require('http-proxy-middleware');
 
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 3000;
 
-const setBeatAPIUrl = 'https://api-beta.setbeat.com/v1/';
+const setBeatAPIUrl = 'http://api-beta.setbeat.com/v1/';
 
 app.use('/', proxy({
     target: setBeatAPIUrl
 }));
 
 
-app.listen(port, () => console.log('the server is running'));
+app.listen(port, () => console.log('the server is running' + port));
